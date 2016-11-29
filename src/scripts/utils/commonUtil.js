64c2e77c9345fc48c9fb3ed9d72df636d29e88ee
@zhangruia <m18611748482@163.com>
2016-11-29 16:55:53 +0800

    add setApp

--- src/scripts/utils/commonUtil.js
+++ src/scripts/utils/commonUtil.js
@@ -1,6 +1,10 @@
 var commonUtil = {
 	setFocus:function($el){
        $el.addClass("active").siblings().removeClass("active");
+	},
+	setApp:function()
+	{
+		console.log("this is 20161129app");
 	}
 }
 
